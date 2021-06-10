054516aa1e40a0d8123309ef6b236c2a6aae9445
Ryan Lee <rlee@edits.net>
2021-06-10 12:14:00 -0700

    fixes flow control

--- main.js
+++ main.js
@@ -8,30 +8,33 @@ const PORTRAIT_HEIGHT = 32;
 
 const dialog_string = "<SFF>Ah!<D3>A shooting star!<*04>";
 
-// Process images early on
-const element_canvas = document.getElementById('output');
-const canvas_ctx = element_canvas.getContext('2d');
-const next_page_images =
-[
-	loadImageAsync("resources/window/next-page.png"),
+//main
+(async () => {
+  // Process images early on
+  const element_canvas = document.getElementById('output');
+  const canvas_ctx = element_canvas.getContext('2d');
+  //grab images
+  const next_page_images = await Promise.all([
+    loadImageAsync("/resources/window/next-page.png"),
 
-	loadImageAsync("resources/window/error-top-next-page.png"),
-	loadImageAsync("resources/window/error-bottom-next-page.png"),
-	loadImageAsync("resources/window/error-both-next-page.png"),
-];
-const last_page_images =
-[
-	loadImageAsync("resources/window/last-page.png"),
+    loadImageAsync("/resources/window/error-top-next-page.png"),
+    loadImageAsync("/resources/window/error-bottom-next-page.png"),
+    loadImageAsync("/resources/window/error-both-next-page.png"),
+  ]);
+  const last_page_images = await Promise.all([
+    loadImageAsync("/resources/window/last-page.png"),
 
-	loadImageAsync("resources/window/error-top-last-page.png"),
-	loadImageAsync("resources/window/error-bottom-last-page.png"),
-	loadImageAsync("resources/window/error-both-last-page.png"),
-];
+    loadImageAsync("/resources/window/error-top-last-page.png"),
+    loadImageAsync("/resources/window/error-bottom-last-page.png"),
+    loadImageAsync("/resources/window/error-both-last-page.png"),
+  ]);
+  const font_default = await loadFontAsync("/resources/fonts/Font.png");
 
-const font_default = loadFontAsync("resources/fonts/Font.png");
+  element_canvas.height = 0;
+  element_canvas.width = 0;
 
-element_canvas.height = 0;
-element_canvas.width = 0;
-//Promise.all(next_page_images).then(images => images.forEach( img => { drawImageAndResizeVertical(element_canvas, img); } ))
-//Promise.all(last_page_images).then(images => images.forEach( img => { drawImageAndResizeVertical(element_canvas, img); } ))
-font_default.then(images => images.forEach( img => { drawImageAndResizeVertical(element_canvas, img); } ));
+  //draw
+  font_default.forEach((img) => {
+    drawImageAndResizeVertical(element_canvas, img);
+  });
+})();
