979e196f693a0d8650699a890351ef9753383e6e
VariantXYZ <variantxyz@gmail.com>
2021-06-10 00:46:47 -0700

    Initial implementation

---
+++ main.js
@@ -0,0 +1,37 @@
+import { loadImageAsync, drawImageAndResizeVertical } from './modules/utils.js';
+import { loadFontAsync } from './modules/font.js'
+
+const DIALOG_WINDOW_WIDTH = 160;
+const DIALOG_WINDOW_HEIGHT = 40;
+const PORTRAIT_WIDTH = 32;
+const PORTRAIT_HEIGHT = 32;
+
+const dialog_string = "<SFF>Ah!<D3>A shooting star!<*04>";
+
+// Process images early on
+const element_canvas = document.getElementById('output');
+const canvas_ctx = element_canvas.getContext('2d');
+const next_page_images =
+[
+	loadImageAsync("resources/window/next-page.png"),
+
+	loadImageAsync("resources/window/error-top-next-page.png"),
+	loadImageAsync("resources/window/error-bottom-next-page.png"),
+	loadImageAsync("resources/window/error-both-next-page.png"),
+];
+const last_page_images =
+[
+	loadImageAsync("resources/window/last-page.png"),
+
+	loadImageAsync("resources/window/error-top-last-page.png"),
+	loadImageAsync("resources/window/error-bottom-last-page.png"),
+	loadImageAsync("resources/window/error-both-last-page.png"),
+];
+
+const font_default = loadFontAsync("resources/fonts/Font.png");
+
+element_canvas.height = 0;
+element_canvas.width = 0;
+//Promise.all(next_page_images).then(images => images.forEach( img => { drawImageAndResizeVertical(element_canvas, img); } ))
+//Promise.all(last_page_images).then(images => images.forEach( img => { drawImageAndResizeVertical(element_canvas, img); } ))
+font_default.then(images => images.forEach( img => { drawImageAndResizeVertical(element_canvas, img); } ));
---
+++ modules/utils.js
@@ -0,0 +1,34 @@
+// General purpose utilities, not specific to the project
+
+async function loadImageAsync(url) 
+{
+  return new Promise(resolve =>
+  {
+    const image = new Image();
+    image.addEventListener('load', () =>
+    {
+      resolve(image);
+    });
+    image.src = url;
+  });
+}
+
+// Expects a perfect fit canvas (height = 0 if nothing is in it yet)
+// This wouldn't be necessary if canvas provided a more convenient way to determine if something was already drawn
+function drawImageAndResizeVertical(element_canvas, img)
+{
+  var context = element_canvas.getContext('2d');
+  var current_width = element_canvas.width
+  var current_height = element_canvas.height;
+  var is_empty = current_height == 0 || current_width == 0
+
+  var data = is_empty ? null : context.getImageData(0, 0, current_width, current_height);
+
+  element_canvas.height += img.height;
+  element_canvas.width = current_width < img.width ? img.width : current_width;
+  if(data) { context.putImageData(data, 0, 0); }
+
+  context.drawImage(img, 0, current_height);
+}
+
+export { loadImageAsync, drawImageAndResizeVertical }
