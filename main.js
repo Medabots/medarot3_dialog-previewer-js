a31f6720254883288b3b64ad7012640de75a8a73
VariantXYZ <variantxyz@gmail.com>
2023-01-01 12:16:25 -0800

    Automatic newlines

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Should be possible to use this for any game as long as 'initialize', 'getDependencies' and 'drawBox' are defined
-import { initialize, getDependencies, drawBox } from './modules/medarot3.js'
+import { initialize, autoLinebreak, getDependencies, drawBox } from './modules/medarot3.js'
 import { assert, drawImageAndResizeVertical } from './modules/utils.js';
 
 //const dialog_string = "<SFF>Ah!<D3>What's that?<D3><D3><@LL,00,00>A shooting star!<*04>";
@@ -8,7 +8,7 @@ import { assert, drawImageAndResizeVertical } from './modules/utils.js';
 //const dialog_string = "Huh? A kid?<CF><D3>I thought it was a shooting<D3>star.<D3>Maybe I can ask them how to<D3>get home.<D3>...but how could they know?<*03>";
 //const dialog_string = "Inserted the <f02><&BUF06><f00> medal.";
 
-const default_dialog_string = "<@RL,0A,01>Include the string to preview by<D3>appending ?t=TEXT to the URL.";
+const default_dialog_string = "<@RL,0A,01>Include the string to preview by appending ?t=TEXT to the URL.";
 const uri_args = window.location.search.substring(1);
 const dialog_string = uri_args && uri_args.includes('t=') ? decodeURIComponent(uri_args.split('t=')[1]) : default_dialog_string;
 
@@ -21,10 +21,13 @@ element_canvas.width = 0;
 let final_images = [];
 initialize().then( () => 
 {
-	for(let dependency of getDependencies(dialog_string))
+	autoLinebreak(dialog_string).then( (formatted_string) =>
 	{
-		final_images.push(drawBox(dependency));
-	}
-	// Wait for all boxes to be prepared and then copy them into the main canvas them in order
-	Promise.all(final_images).then((images) => { images.forEach( image => { drawImageAndResizeVertical(element_canvas, image); }); });
+		for(let dependency of getDependencies(formatted_string))
+		{
+			final_images.push(drawBox(dependency));
+		}
+		// Wait for all boxes to be prepared and then copy them into the main canvas them in order
+		Promise.all(final_images).then((images) => { images.forEach( image => { drawImageAndResizeVertical(element_canvas, image); }); });	
+	});
 });
